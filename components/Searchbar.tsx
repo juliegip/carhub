e00487b093a0508d568a30e1a9b1b86688d1f579
juliegip <julie.gip@gmail.com>
2024-01-17 10:11:53 +0100

    feat: custom filer SearchParams

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -5,7 +5,6 @@ import { useRouter } from "next/navigation"
 
 import { SearchManufacturer } from "."
 import Image from "next/image"
-import { Router } from "next/router"
 
 const SearchButton = ({otherClasses} : {otherClasses:string} ) => (
   <button type="submit" className={`-ml3 z-10 ${otherClasses}`}>
